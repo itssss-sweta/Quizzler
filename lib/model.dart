7b28b1a077b6f21307d2cab4b3e62cddd14aa4a4
itssss-sweta <swetamhrjn252@gmail.com>
2023-07-08 22:20:20 +0545

    this keyword used

--- lib/model.dart
+++ lib/model.dart
@@ -1,9 +1,6 @@
 class Question {
-  late String questionText;
-  late bool questionAnswer;
+  String questionText;
+  bool questionAnswer;
 
-  Question({required String q, required bool a}) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question({required this.questionText, required this.questionAnswer});
 }
--- lib/quiz_bran.dart
+++ lib/quiz_bran.dart
@@ -4,33 +4,50 @@ class QuizBrain {
   int _questionNumber = 0;
 
   final List<Question> _questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
     Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-    Question(q: 'Some cats are actually allergic to humans', a: true),
-    Question(q: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', a: true),
-    Question(q: 'It is illegal to pee in the Ocean in Portugal.', a: true),
+        questionText: 'You can lead a cow down stairs but not up stairs.',
+        questionAnswer: false),
     Question(
-        q: 'No piece of square dry paper can be folded in half more than 7 times.',
-        a: false),
+        questionText:
+            'Approximately one quarter of human bones are in the feet.',
+        questionAnswer: true),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
     Question(
-        q: 'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        a: true),
+        questionText: 'Some cats are actually allergic to humans',
+        questionAnswer: true),
     Question(
-        q: 'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        a: false),
+        questionText: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+        questionAnswer: true),
     Question(
-        q: 'The total surface area of two human lungs is approximately 70 square metres.',
-        a: true),
-    Question(q: 'Google was originally called \"Backrub\".', a: true),
+        questionText: 'It is illegal to pee in the Ocean in Portugal.',
+        questionAnswer: true),
     Question(
-        q: 'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        a: true),
+        questionText:
+            'No piece of square dry paper can be folded in half more than 7 times.',
+        questionAnswer: false),
     Question(
-        q: 'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        a: true),
+        questionText:
+            'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        questionAnswer: true),
+    Question(
+        questionText:
+            'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        questionAnswer: false),
+    Question(
+        questionText:
+            'The total surface area of two human lungs is approximately 70 square metres.',
+        questionAnswer: true),
+    Question(
+        questionText: 'Google was originally called \"Backrub\".',
+        questionAnswer: true),
+    Question(
+        questionText:
+            'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        questionAnswer: true),
+    Question(
+        questionText:
+            'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        questionAnswer: true),
   ];
 
   void nextQuestion() {
